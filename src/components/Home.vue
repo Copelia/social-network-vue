76d9577f6455c9e9f4ef9e6fd3dcf173837e6fa5
[Copelia] <iamcorallium@gmail.com>
2018-09-27 16:55:47 -0500

    Añadiendo elementos a base de datos de firebase y función created() que necesita refactorización

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,11 @@
     <!-- <h2 class="display-3">{{ msg }}</h2> -->
     <h3>Bike Workshops Network</h3>
     <p class="lead">Comparte y encuentra los talleres mecánicos rifados en la CDMX!</p>
-    <input type="text" v-model='workshop' @keyup.enter='addWorkshop'>
+    <input type="text" v-model='workshop' @keyup.enter='created'>
       <ul>
-        <li v-for='workshopName in workshops' :key='workshopName' >
-          {{workshopName}}
+        <li v-for='(workshopName, key) in workshops' :key='key' >
+          {{workshopName.name}}
+          <button>Edit</button>
         </li>
       </ul>
     <button class="btn btn-dark" v-on:click="logout">Cierra Sesión</button>
@@ -20,13 +21,22 @@ export default {
   data () {
     return {
       workshop: null,
-      workshops: ['Mr.Bueno', 'BeSpoke']
+      workshops: {}
       // msg: 'Bienvenido/a a Garnachapp'
     }
   },
   methods: {
     addWorkshop () {
-      this.workshops.push(this.workshop)
+      // this.workshops.push(this.workshop)
+      firebase.database().ref('workshops').push({name: this.workshop})
+        .then((data) => [console.log(data)])
+        .catch((error) => [console.log(error)])
+    },
+    created () {
+      firebase.database().ref('workshops').on('value', (snapshot) => {
+        // console.log(snapshot.val())
+        this.workshops = snapshot.val()
+      })
     },
 
     logout () {
